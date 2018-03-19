4f78391cbb8cbe5d52685e27fecc6e9729d3f2d7
Vignesh <vettyvignesh@gmail.com>
2018-03-20 03:51:37 +0530

    UI of the validate application now feels modern (#54)

--- src/org/opendatakit/validate/FormValidator.java
+++ src/org/opendatakit/validate/FormValidator.java
@@ -44,6 +44,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.javarosa.core.model.Constants;
@@ -120,12 +122,28 @@ public class FormValidator implements ActionListener {
 
 
     public static void main(String[] args) {
-        if ( args.length == 1 ) {
-            String path = args[0];
-            new FormValidator().validateAndExitWithErrorCode(path);
-        } else {
-            new FormValidator().show();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            if (args.length == 1) {
+                String path = args[0];
+                new FormValidator().validateAndExitWithErrorCode(path);
+            } else {
+                new FormValidator().show();
+            }
         }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
