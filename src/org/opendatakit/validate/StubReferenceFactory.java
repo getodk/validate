e9286bbca4efae914d6e91d669ee4ba7ffd53e71
Hélène Martin <lognaturel@gmail.com>
2019-06-06 00:51:46 -0700

    Add stubs so external secondary instances can pass validation (#66)

---
+++ src/org/opendatakit/validate/StubReferenceFactory.java
@@ -0,0 +1,26 @@
+package org.opendatakit.validate;
+
+import org.javarosa.core.reference.Reference;
+import org.javarosa.core.reference.ReferenceFactory;
+
+/**
+ * Always provides a reference to the same simple XML document. This allows forms with external secondary instances to
+ * pass validation.
+ */
+public class StubReferenceFactory implements ReferenceFactory {
+
+    @Override
+    public boolean derives(String URI) {
+        return true;
+    }
+
+    @Override
+    public Reference derive(String URI) {
+        return new StubReference();
+    }
+
+    @Override
+    public Reference derive(String URI, String context) {
+        return new StubReference();
+    }
+}
