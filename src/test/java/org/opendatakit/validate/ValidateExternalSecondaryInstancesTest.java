23f850d5fca0b56f1eb48a64253e821e9a06bffc
Guille <ggalmazor@gmail.com>
2019-06-11 18:13:31 +0200

    Add a test that verifies narrow support for external secondary instances - Only supports itemset definitions like the ones generated by xslforms, which use <value ref="name"/> and <label ref="label"/>

---
+++ src/test/java/org/opendatakit/validate/ValidateExternalSecondaryInstancesTest.java
@@ -0,0 +1,70 @@
+package org.opendatakit.validate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.isEmptyString;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+public class ValidateExternalSecondaryInstancesTest {
+    @Test
+    public void supports_xlsforms_value_and_label_refs_in_itemsets_using_secondary_external_instances() throws URISyntaxException {
+        final Path path = getPathOf("external_secondary_instance_non_standard_use.xml");
+        final FormValidator validator = new FormValidator();
+
+        Output output = Output.runAndGet(new Runnable() {
+            @Override
+            public void run() {
+                validator.validate(path.toString());
+            }
+        });
+        assertThat(output.err, isEmptyString());
+        assertThat(output.std, containsString("Xform is valid"));
+    }
+
+    private Path getPathOf(String filename) throws URISyntaxException {
+        return Paths.get(ValidateExternalSecondaryInstancesTest.class.getResource(filename.startsWith("/") ? filename : "/" + filename).toURI());
+    }
+
+    static class Output {
+        private final String std;
+        private final String err;
+
+        Output(String std, String err) {
+            this.std = std;
+            this.err = err;
+        }
+
+        static Output runAndGet(Runnable runnable) {
+            PrintStream outBackup = System.out;
+            ByteArrayOutputStream stdBaos = new ByteArrayOutputStream();
+            PrintStream stdPs = new PrintStream(stdBaos);
+            System.setOut(stdPs);
+
+            PrintStream errBackup = System.err;
+            ByteArrayOutputStream errBaos = new ByteArrayOutputStream();
+            PrintStream errPs = new PrintStream(errBaos);
+            System.setErr(errPs);
+
+            runnable.run();
+
+            stdPs.flush();
+            String std = stdBaos.toString();
+            System.setOut(outBackup);
+            System.out.print(std);
+
+            errPs.flush();
+            String err = errBaos.toString();
+            System.setErr(errBackup);
+            System.err.print(err);
+
+            return new Output(std, err);
+        }
+    }
+
+}
